f22909f75da572a0c20e7794d229a466022a8afd
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-22 17:23:26 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    addToDo(e.target.taskDescription.value)
+    form.reset()
+  })
 });
+
+function addToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#list').appendChild(li)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
